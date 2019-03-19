fd12ceafcac728d787ba810907eed7ce75ec47ac
吴国栋 <1158732677@qq.com>
2019-03-19 15:47:33 +0800

    tijiao4

--- src/main/java/com/example/springbootdemo/MyProperties.java
+++ src/main/java/com/example/springbootdemo/MyProperties.java
@@ -6,14 +6,15 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationProperties(prefix = "my")
 public class MyProperties {
-	
+
+	//huncun1
     private String name;
     private String age;
-
+    
     public String getName() {
         return name;
     }
-
+    
     public void setName(String name) {
         this.name = name;
     }
